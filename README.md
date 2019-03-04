b0ae70f50aa1d3bcd4a808b3b6873855a47210a8
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2019-03-04 15:34:44 +0100

    script to check prerequisites

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Cloud Native Starter for Java and Node.js
 
+This project contains sample code that shows how to build cloud-native applications with Java and Node.js and deploy them to Kubernetes and Istio.
+
+The project showcases the following functionality:
+
+* Chained invocations
+* Traffic management (A/B, canary)
+* Polyglot Java and JavaScript microservices
+* Circuit breaker and fallbacks
+* Distributed logging
+* Metrics
+* Authentication and authorization
+* APIs incl. documentation
+* Configuration
+* Deployments (helm, yaml)
+
 ### Work in Progress
 
 Stay tuned for more ...
@@ -15,6 +30,7 @@ Follow these [instructions](LocalEnvironment.md) to set up the local environment
 Deploy and redeploy:
 
 ```
+$ scripts/check-prerequisites.sh
 $ scripts/deploy-articles-java-jee.sh
 $ scripts/deploy-web-api-java-jee.sh
 ```
---
+++ scripts/check-prerequisites.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+root_folder=$(cd $(dirname $0); cd ..; pwd)
+
+exec 3>&1
+
+function _out() {
+  echo "$(date +'%F %H:%M:%S') $@"
+}
+
+function checkPrerequisites() {
+    MISSING_TOOLS=""
+    git --version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} git"
+    curl --version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} curl"
+    java -version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} java"
+    mvn -version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} mvn"
+    node -v &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} node"
+    npm -v &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} npm"
+    docker -v &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} docker"
+    kubectl version /dev/null || MISSING_TOOLS="${MISSING_TOOLS} kubectl"
+    minikube version || MISSING_TOOLS="${MISSING_TOOLS} minikube"
+    if [[ -n "$MISSING_TOOLS" ]]; then
+      _out "Some tools (${MISSING_TOOLS# }) could not be found, please install them first"
+      exit 1
+    else
+      _out You have all necessary prerequisites installed
+    fi
+}
+
+checkPrerequisites
