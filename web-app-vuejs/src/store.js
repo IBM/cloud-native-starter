a3a2ee5438dde961ba9d96b42d4962cfc52c2c1f
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2019-04-06 15:38:52 +0200

    default store.js

--- web-app-vuejs/src/store.js
+++ web-app-vuejs/src/store.js
@@ -5,10 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    authenticationEnabled: "authentication-enabled-yes", // use "authentication-enabled-yes" to turn it on
+    authenticationEnabled: "authentication-enabled-no", // use "authentication-enabled-yes" to turn it on
     endpoints: {
-      api: "http://192.168.99.100:31380/web-api/v1/", // example: "http://192.168.99.100:31380/web-api/v1/"
-      login: "http://192.168.99.100:31380/login" // example: "http://localhost:3000/login"
+      api: "http://endpoint-api-ip:31380/web-api/v1/", // example: "http://192.168.99.100:31380/web-api/v1/"
+      login: "http://endpoint-login-ip:endpoint-login-port/login" // example: "http://localhost:3000/login"
     },
     user: {
       isAuthenticated: false,
