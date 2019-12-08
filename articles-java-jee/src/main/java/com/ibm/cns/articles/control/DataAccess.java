24eada9b2757a11d0bd54a60fe4eafe1ff12d230
Adam Bien <abien@adam-bien.com>
2019-12-08 15:25:29 +0100

    superfluous exception declarations removed

--- articles-java-jee/src/main/java/com/ibm/cns/articles/control/DataAccess.java
+++ articles-java-jee/src/main/java/com/ibm/cns/articles/control/DataAccess.java
@@ -1,14 +1,13 @@
 package com.ibm.cns.articles.control;
 
+import com.ibm.cns.articles.entity.Article;
 import java.util.List;
 
-import com.ibm.cns.articles.entity.Article;
-import com.ibm.cns.articles.entity.Article;
-
 public interface DataAccess {
-	public Article addArticle(Article article) throws NoConnectivity;
 
-    public Article getArticle(String id) throws NoConnectivity, ArticleDoesNotExist;
-    
-    public List<Article> getArticles() throws NoConnectivity;
+    public Article addArticle(Article article);
+
+    public Article getArticle(String id);
+
+    public List<Article> getArticles();
 }
