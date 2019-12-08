93c65cd40d8b9bcb2cc65f560222272b977eb190
Adam Bien <abien@adam-bien.com>
2019-12-08 14:54:00 +0100

    superfluous accessors removed

--- articles-java-jee/src/main/java/com/ibm/cns/articles/entity/Article.java
+++ articles-java-jee/src/main/java/com/ibm/cns/articles/entity/Article.java
@@ -1,11 +1,63 @@
 package com.ibm.cns.articles.entity;
 
-public class Article {
+import java.io.Serializable;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
-	public String title;
-	public String url;
+@Entity(name = "Article")
+@Table(name = "Article")
+@NamedQuery(name = "Article.findAll", query = "SELECT e FROM Article e")
+@NamedQuery(name = "Article.findArticle", query = "SELECT a FROM Article a WHERE "
+    + "a.title = :title AND a.url = :url AND a.author = :author")
+public class Article implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "articleId")
+    public int id;
+
+    @Column(name = "articleTitle")
+    public String title;
+
+    @Column(name = "articleUrl")
+    public String url;
+
+    @Column(name = "articleAuthor")
     public String author;
-    public String id;
+
+    @Column(name = "creationDate")
     public String creationDate;
+
+    public Article() {
+    }
+    
+    public Article(String title, String url, String author, String creationDate) {
+        this.title = title;
+        this.url = url;
+        this.author = author;
+        this.creationDate = creationDate;
+    }
+
+    public JsonObject toJSON() {
+        return Json.createObjectBuilder().add("id", this.id).add("title", this.title).add("url", this.url)
+                .add("author", this.author).build();
+    }
+
+    public String getIDAsString() {
+        return String.valueOf(this.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Article [title=" + title + ", url=" + url + ", author=" + author + "]";
+    }
 
 }
