f77c97e432994a6acaae0a7050e44787352e346b
Adam Bien <abien@adam-bien.com>
2019-12-08 14:43:01 +0100

    Equals and hashCode are not necessary in entities

--- articles-java-jee/src/main/java/com/ibm/cns/articles/entity/ArticleEntity.java
+++ articles-java-jee/src/main/java/com/ibm/cns/articles/entity/ArticleEntity.java
@@ -1,13 +1,13 @@
 package com.ibm.cns.articles.entity;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.NamedQuery;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 @Entity(name = "Article")
 @Table(name = "Article")
@@ -82,50 +82,6 @@ public class ArticleEntity implements Serializable {
 
     public String getAuthor() {
         return author;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        ArticleEntity other = (ArticleEntity) obj;
-        if (url == null) {
-            if (other.url != null) {
-                return false;
-            }
-        } else if (!url.equals(other.url)) {
-            return false;
-        }
-        if (title == null) {
-            if (other.title != null) {
-                return false;
-            }
-        } else if (!title.equals(other.title)) {
-            return false;
-        }
-        if (creationDate == null) {
-            if (other.creationDate != null) {
-                return false;
-            }
-        } else if (!creationDate.equals(other.creationDate)) {
-            return false;
-        }
-        if (author == null) {
-            if (other.author != null) {
-                return false;
-            }
-        } else if (!author.equals(other.author)) {
-            return false;
-        }
-
-        return true;
     }
 
     @Override
