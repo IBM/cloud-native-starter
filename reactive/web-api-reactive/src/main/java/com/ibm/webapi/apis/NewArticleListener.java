19cc8f550280c4d7e9060a4df1b533ac122fdfa2
Niklas Heidloff <nheidloff@gmail.com>
2019-12-17 17:51:40 +0100

    improved article listener

--- reactive/web-api-reactive/src/main/java/com/ibm/webapi/apis/NewArticleListener.java
+++ reactive/web-api-reactive/src/main/java/com/ibm/webapi/apis/NewArticleListener.java
@@ -4,32 +4,19 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import javax.enterprise.context.ApplicationScoped;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
-import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.annotations.Emitter;
-import javax.inject.Inject;
 
 @ApplicationScoped
 public class NewArticleListener {
 
-    @Inject
-    @Channel("workaround-channel") Emitter<String> emitter;
-
-    @Incoming("workaround-channel")
+    @Incoming("new-article-created")
     @Outgoing("my-data-stream")
     @Broadcast
-    public String processWorkaround(String articleId) {
-        System.out.println("Channel message received: workaround-channel - " + articleId);
+    public String process(String articleId) {
+        System.out.println("Kafka message received: new-article-created - " + articleId);
         return articleId;
     }
-
-    @Incoming("new-article-created")
-    public void process(String articleId) {
-        System.out.println("Kafka message received: new-article-created - " + articleId);
-        emitter.send(articleId);
-    }
-
+    
     @Incoming("my-data-stream")
-    public void processAnotherWorkaround(String articleId) {
+    public void processWorkaround(String articleId) {
     }
-
 }
