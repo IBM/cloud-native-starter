1a8983c0c6f234869c6deb704d5b648e3506eb39
ochamber <ochamber@users.noreply.github.com>
2019-12-18 09:47:43 +0100

    Update create-db2.sh
    Changed for IBM Cloud dashDB (dashdb-for-transactions) service instead of CF

--- ibm-scripts/create-db2.sh
+++ ibm-scripts/create-db2.sh
@@ -32,33 +32,55 @@ function check_tools() {
 function ibmcloud_login() {
   ibmcloud config --check-version=false
   ibmcloud api --unset
+  #CHO Should be something like this
+  #ibmcloud api $IBM_CLOUD_API
+  #instead of ibmcloud api https://cloud.ibm.com but anymay
   ibmcloud api https://cloud.ibm.com
-  ibmcloud login --apikey $IBMCLOUD_API_KEY -r $IBM_CLOUD_REGION
-
-  ibmcloud target --cf-api $IBM_CLOUD_CF_API -o $IBM_CLOUD_CF_ORG -s $IBM_CLOUD_CF_SPACE
+  #ibmcloud login --apikey $IBMCLOUD_API_KEY -r $IBM_CLOUD_REGION
+  ibmcloud login --apikey $IBMCLOUD_API_KEY -r eu-gb
+  #CHO Force region to be eu-gb as Lite plan for dashDB is not available in Germany! (only gb & us-south)
+  #RG=$(ibmcloud resource groups --default | grep -i ^default | awk '{print $1}')
+  ibmcloud target -g $IBM_CLOUD_RG
+  #ibmcloud target --cf-api $IBM_CLOUD_CF_API -o $IBM_CLOUD_CF_ORG -s $IBM_CLOUD_CF_SPACE
+  #no need for cf services anymore
 }
 
 function setup() {
   _out Creating db2 service instance
   
-  ibmcloud service create 'dashDB For Transactions' Lite db2-cloud-native-starter 
+  #CHO 
+  #ibmcloud service create 'dashDB For Transactions' Lite db2-cloud-native-starter 
+  ibmcloud resource service-instance-create db2-cloud-native-starter 'dashdb-for-transactions' free  eu-gb -p '{"location": "$IBM_CLOUD_REGION", "hipaa": "false", "legacyCredentials": true}'
+  #ibmcloud resource service-instance-delete -f db2-cloud-native-starter
   
-  ibmcloud service key-create db2-cloud-native-starter 'dashDB For Transactions'
-  DB2_SERVER_NAME=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/hostname/{ print $2 }')
-  DB2_SERVER_NAME=${DB2_SERVER_NAME//\"/$''}
-  DB2_SERVER_NAME=${DB2_SERVER_NAME//,/$''}
+  #ibmcloud service key-create db2-cloud-native-starter 'dashDB For Transactions'
+  ibmcloud resource service-key-create cred4db2-cloud-native-starter Manager --instance-name db2-cloud-native-starter
+  #ibmcloud resource service-key-delete -f cred4db2-cloud-native-starter
+  
+  #Original : 3 lines
+  #DB2_SERVER_NAME=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/hostname/{ print $2 }')
+  #DB2_SERVER_NAME=${DB2_SERVER_NAME//\"/$''}
+  #DB2_SERVER_NAME=${DB2_SERVER_NAME//,/$''}
+  DB2_SERVER_NAME=$(ibmcloud resource service-key cred4db2-cloud-native-starter | awk '/hostname/{ print $2 }')
+  #DB2_SERVER_NAME=${DB2_SERVER_NAME//\"/$''}
+  #DB2_SERVER_NAME=${DB2_SERVER_NAME//,/$''}
   printf "\nDB2_SERVER_NAME=$DB2_SERVER_NAME" >> $ENV_FILE
-  DB2_PORT=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/port/{ print $2 }')
-  DB2_PORT=${DB2_PORT//\"/$''}
-  DB2_PORT=${DB2_PORT//,/$''}
+  #DB2_PORT=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/port/{ print $2 }')
+  #DB2_PORT=${DB2_PORT//\"/$''}
+  #DB2_PORT=${DB2_PORT//,/$''}
+  DB2_PORT=$(ibmcloud resource service-key cred4db2-cloud-native-starter | awk '/port/{ print $2 }')
+  #DB2_PORT=${DB2_PORT//\"/$''}
+  #DB2_PORT=${DB2_PORT//,/$''}
   printf "\nDB2_PORT=$DB2_PORT" >> $ENV_FILE
-  DB2_USER=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/username/{ print $2 }')
-  DB2_USER=${DB2_USER//\"/$''}
-  DB2_USER=${DB2_USER//,/$''}
+  #DB2_USER=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/username/{ print $2 }')
+  DB2_USER=$(ibmcloud resource service-key cred4db2-cloud-native-starter | awk '/username/{ print $2 }')
+  #DB2_USER=${DB2_USER//\"/$''}
+  #DB2_USER=${DB2_USER//,/$''}
   printf "\nDB2_USER=$DB2_USER" >> $ENV_FILE
-  DB2_PASSWORD=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/password/{ print $2 }')
-  DB2_PASSWORD=${DB2_PASSWORD//\"/$''}
-  DB2_PASSWORD=${DB2_PASSWORD//,/$''}
+  #DB2_PASSWORD=$(ibmcloud service key-show db2-cloud-native-starter 'dashDB For Transactions' | awk '/password/{ print $2 }')
+  DB2_PASSWORD=$(ibmcloud resource service-key cred4db2-cloud-native-starter | awk '/password/{ print $2 }')
+  #DB2_PASSWORD=${DB2_PASSWORD//\"/$''}
+  #DB2_PASSWORD=${DB2_PASSWORD//,/$''}
   printf "\nDB2_PASSWORD=$DB2_PASSWORD" >> $ENV_FILE
 }
 
