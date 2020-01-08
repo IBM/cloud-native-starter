5cd827ab1f3132bfef6bfd43582ee12915f631e4
Niklas Heidloff <nheidloff@gmail.com>
2020-01-08 17:58:02 +0100

    timing issue

--- reactive/scripts/deploy-postgres-database.sh
+++ reactive/scripts/deploy-postgres-database.sh
@@ -14,6 +14,8 @@ function setup() {
   cd ${root_folder}/scripts
   kubectl create -f postgres-cluster.yaml -n my-postgresql-operator-dev4devs-com
 
+  sleep 10
+
   kubectl patch svc database-articles -n my-postgresql-operator-dev4devs-com -p '{"spec": {"ports": [{"port": 5432,"targetPort": 5432,"name": "database-articles", "protocol": "TCP"}],"type": "NodePort"}}'
   
   _out Done deploying postgres cluster
