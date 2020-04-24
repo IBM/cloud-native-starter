360632a7d2d6f095138236c1a99fafc33fc14e4d
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-04-24 14:37:00 +0200

    minor fix

--- reactive/os4-scripts/deploy-web-app-reactive-via-oc.sh
+++ reactive/os4-scripts/deploy-web-app-reactive-via-oc.sh
@@ -29,9 +29,6 @@ function setup() {
     oc new-build --name web-app-reactive --binary --strategy docker
     oc start-build web-app-reactive --from-dir=.
 
-    mv Dockerfile.k8s Dockerfile
-    rm Dockerfile.os4
-    
     sed -e "s+web-app-reactive:latest+image-registry.openshift-image-registry.svc:5000/cloud-native-starter/web-app-reactive:latest+g" \
       -e "s+  type: NodePort+\#  type: NodePort+g" \
       -e "s+        imagePullPolicy: Never+\#        imagePullPolicy: Never+g" \
