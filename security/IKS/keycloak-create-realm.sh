ce71ac5edc64ac6de518332dedf11df21465052a
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2020-08-04 16:39:36 +0200

    update security keycloak-create-realm.sh

--- security/IKS/keycloak-create-realm.sh
+++ security/IKS/keycloak-create-realm.sh
@@ -7,17 +7,24 @@ GRANT_TYPE=password
 CLIENT_ID=admin-cli
 #INGRESSURL="YOUR URL"
 
+echo "------------------------------------------------------------------------"
+echo "Your INGRESSURL for Keycloak: $INGRESSURL"
+echo "------------------------------------------------------------------------"
+echo ""
+
 # Get the bearer token from Keycloak
 echo "------------------------------------------------------------------------"
 echo "Get the bearer token from Keycloak"
-access_token=$( curl -d "client_id=$CLIENT_ID" -d "username=$USER" -d "password=$PASSWORD" -d "grant_type=$GRANT_TYPE" "$INGRESSURL/auth/realms/master/protocol/openid-connect/token" | sed -n 's|.*"access_token":"\([^"]*\)".*|\1|p')
 echo "------------------------------------------------------------------------"
+echo ""
+access_token=$( curl -d "client_id=$CLIENT_ID" -d "username=$USER" -d "password=$PASSWORD" -d "grant_type=$GRANT_TYPE" "$INGRESSURL/auth/realms/master/protocol/openid-connect/token" | sed -n 's|.*"access_token":"\([^"]*\)".*|\1|p')
 
 # Create the realm in Keycloak
 echo "------------------------------------------------------------------------"
 echo "Create the realm in Keycloak"
 echo "------------------------------------------------------------------------"
- 
+echo ""
+
 result=$(curl -d @../BackupFiles/quarkus-realm.json -H "Content-Type: application/json" -H "Authorization: bearer $access_token" "$INGRESSURL/auth/admin/realms")
 
 if [ "$result" = "" ]; then
