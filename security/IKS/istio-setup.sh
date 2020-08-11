b8dd63f904092deede5ff72d4f660176095f2357
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2020-08-11 14:18:22 +0200

    update security istio-setup.sh

--- security/IKS/istio-setup.sh
+++ security/IKS/istio-setup.sh
@@ -44,7 +44,7 @@ function setuptelemetry {
  kubectl delete svc kiali -n istio-system
  kubectl delete svc prometheus -n istio-system
  kubectl delete svc jaeger-query -n istio-system
- kubectl apply -f istio-tele-services.yaml
+ kubectl apply -f IKS/istio-tele-services.yaml
  echo "------------------------------------------------------------------------"
  echo "Label namespace 'default' for auto injection"
  kubectl label namespace default istio-injection=enabled
